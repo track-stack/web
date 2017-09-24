82e777df8071c937d3344d8e406b72d705f15d73
Mike Kavouras <kavourasm@gmail.com>
2017-09-24 18:11:44 -0500

    get that root looking decent

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
-    scope: "email", callback_url: ENV['FACEBOOK_CALLBACK_URL']
+    scope: "email", callback_url: ENV['FACEBOOK_CALLBACK_URL'], image_size: "large"
 end
