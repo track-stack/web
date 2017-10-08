94adb38fd06f9cf8626b49eac1be6912bca110c1
Mike Kavouras <kavourasm@gmail.com>
2017-10-08 14:17:04 -0500

    user_friends permissions and game routes

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+  def new
+
+  end
+
+  def show
+  end
+
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   root to: "pages#index"
+
+  resources "games", only: [:new, :show, :create]
 end
