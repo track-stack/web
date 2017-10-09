fcb380aafd7233503767c67bfc54f802ec32a211
Mike Kavouras <kavourasm@gmail.com>
2017-10-09 00:44:41 -0500

    render 401 if no current user

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -5,6 +5,8 @@ class FriendsController < ApplicationController
     if current_user
       friends = ::Facebook::FriendFinder.all_for(user: current_user)
       render json: { friends: friends }
+    else
+      render file: "#{Rails.root}/public/401", layout: false, status: 401, formats: [:html]
     end
   end
 end
