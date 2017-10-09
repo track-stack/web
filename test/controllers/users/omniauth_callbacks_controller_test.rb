ed3dd82661f1d0468ffbf11f144aab989dd476d9
Mike Kavouras <kavourasm@gmail.com>
2017-10-09 10:52:15 -0500

    add test for auth has without email

--- test/controllers/users/omniauth_callbacks_controller_test.rb
+++ test/controllers/users/omniauth_callbacks_controller_test.rb
@@ -12,14 +12,34 @@ class Users::OmniauthCallbacksControllerTest < ActionController::TestCase
     end
   end
 
-  setup do
+  def auth_hash_without_email
+    @auth_hash ||= begin
+      file_path = Rails.root.join("test", "fixtures", "facebook_omniauth_hash.json")
+      file = File.read(file_path)
+      json = JSON.parse(file)
+      json.delete :email
+      OmniAuth::AuthHash.new(json)
+    end
+  end
+
+  test "creates a new user" do
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new auth_hash
     @request.env["devise.mapping"] = Devise.mappings[:user]
     @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+
+    assert_difference "User.count" do
+      post "facebook"
+    end
+    assert_redirected_to root_path
   end
 
-  test "creates a new user" do
+  test "creates a user without an email" do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new auth_hash_without_email
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+
     assert_difference "User.count" do
       post "facebook"
     end
@@ -27,6 +47,11 @@ class Users::OmniauthCallbacksControllerTest < ActionController::TestCase
   end
 
   test "does not create a user if one exists" do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new auth_hash
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+
     user = User.from_omniauth OmniAuth::AuthHash.new(auth_hash)
     post "facebook"
 
