9343af088ab40f12c77f83eeca082e7c7457c6e1
Mike Kavouras <kavourasm@gmail.com>
2017-10-09 10:36:20 -0500

    apparently emails aren't always present

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,19 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      user.email = auth.info.email || random_email
       user.password = Devise.friendly_token[0,20]
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.name = auth.info.name
       user.image = auth.info.image
     end
+  end
+
+  private
+
+  def self.random_email
+    name = (0...10).map { ('a'..'z').to_a[rand(26)] }.join
+    "#{name}@email.com"
   end
 end
