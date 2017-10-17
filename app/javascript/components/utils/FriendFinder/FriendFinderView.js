c35ed75085962a07f31529b547ce42208e63d9b9
Mike Kavouras <kavourasm@gmail.com>
2017-10-17 18:32:48 -0500

    select friends

--- app/javascript/components/utils/FriendFinder/FriendFinderView.js
+++ app/javascript/components/utils/FriendFinder/FriendFinderView.js
@@ -4,7 +4,7 @@ export default class FriendFinderView extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = { friends: [] }
+    this.state = { friends: [], selectedFriend: null }
 
     // fetch friends!
     this.props.fetchFriends()
@@ -25,29 +25,58 @@ export default class FriendFinderView extends React.Component {
     this.setState({ friends: friends })
   }
 
+  handleClick(friend) {
+    this.setState({ selectedFriend: friend })
+  }
+
   render() {
     const friends = this.state.friends.map((friend, idx) => {
       return (
-        <li key={idx}>
+        <li key={idx} onClick={ () => { this.handleClick(friend) }}>
           <img src={friend.picture.data.url} width="30" height="30" />
           {friend.name}
         </li>
-    )
+      )
     })
 
-    return (
-      <div className="friends-list">
-        <div style={{marginTop: 30}}>
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Find friends"
-            onChange={this.queryChanged}
-          />
-        </div>
+    const friendsList = (
+      <div className="friends-list selectable">
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Find friends"
+          onChange={this.queryChanged}
+        />
         <ul className="list-unstyled">
           {friends}
         </ul>
+      </div>
+    )
+
+    const invitee = this.state.selectedFriend ? (
+      <div className="invite-list">
+        <h4 style={{marginTop: 0}}>Send invitation</h4>
+        <div>
+          <img src={this.state.selectedFriend.picture.data.url} width="30" height="30" />
+          <span>{this.state.selectedFriend.name}</span>
+        </div>
+        <form action="/games/create" method="post">
+          <input name="invitee_uid" value={this.state.selectedFriend.id} type="hidden" />
+          <input type="submit" className="btn btn-success" value="Send" />
+        </form>
+      </div>
+    ) : null;
+
+    return (
+      <div className="row">
+        <div style={{marginTop: 30}}>
+          <div className="col-xs-8">
+            {friendsList}
+          </div>
+          <div className="col-xs-4">
+            {invitee}
+          </div>
+        </div>
       </div>
     )
   }
