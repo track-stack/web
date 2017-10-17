1b3c603cdd3cc7ee3d12d24ae8c6de2436ea688f
Mike Kavouras <kavourasm@gmail.com>
2017-10-17 14:34:40 -0500

    display pending invitations

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,7 @@
 class GamesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new]
+
   def new
   end
 end
--- app/models/game_invitation.rb
+++ app/models/game_invitation.rb
@@ -1,9 +1,6 @@
 class GameInvitation < ApplicationRecord
   enum status: [:pending, :accepted]
 
-  scope :pending, -> { where(status: 0) }
-  scope :accepted, -> { where(status: 1) }
-
   def accept
     update_attribute(:status, 1)
   end
