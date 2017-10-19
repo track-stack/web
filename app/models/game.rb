040d100f14cf8418f22205597552e9edf151261a
Mike Kavouras <kavourasm@gmail.com>
2017-10-18 20:21:04 -0500

    add user <> game relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,13 @@
 class Game < ApplicationRecord
+
+  def self.from(invite:, invitee:)
+    ActiveRecord::Base.transaction do
+      invite.accept!
+      game = Game.create
+      UserGame.create(user_id: invitee.id, game_id: game.id, creator: false)
+      UserGame.create(user_id: invite.inviter_id, game_id: game.id, creator: true)
+
+      game
+    end
+  end
 end
