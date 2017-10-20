0b9811df7d6c6a4babeb0917ea49aa9e6ee64e78
Mike Kavouras <kavourasm@gmail.com>
2017-10-20 14:49:31 -0500

    add image to user serializer

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe UserSerializer do
     user = create(:user, :facebook)
     serialized_user_json = JSON.parse(UserSerializer.new(user).to_json)
 
-    expect(serialized_user_json.keys).to eq(["id", "name"])
+    expect(serialized_user_json.keys).to eq(["id", "name", "image"])
     expect(serialized_user_json["name"]).to eq(user.name)
     expect(serialized_user_json["id"]).to eq(user.id)
   end
