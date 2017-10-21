61c951115eaa9ef4469d6d65db5253c76aa7c6ed
Mike Kavouras <kavourasm@gmail.com>
2017-10-21 17:28:35 -0500

    authenticate_user! prefer root over sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::Base
   def render_401
     render file: "#{Rails.root}/public/401.html", layout: false, status: 401
   end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      hand = ["✋🏻", "✋🏼", "✋🏽", "✋🏾", "✋🏿"].sample
+      flash[:error] = "#{hand} You need to log in to see that"
+      redirect_to :root
+    end
+  end
 end
