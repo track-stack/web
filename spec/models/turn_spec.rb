6ad6813153efadfa0f352be17d1e389d20c28034
Mike Kavouras <kavourasm@gmail.com>
2017-10-21 16:33:14 -0500

    mark game as playing after commit hook

--- spec/models/turn_spec.rb
+++ spec/models/turn_spec.rb
@@ -3,4 +3,20 @@ require 'rails_helper'
 RSpec.describe Turn, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:game) }
+
+  context "after_create" do
+    it "updates game status" do
+      user = create(:user, :facebook)
+      user_2 = create(:user, :facebook)
+      game = create(:game, status: 0)
+
+      expect(game.playing?).to be false
+
+      turn = create(:turn, game: game, user: user)
+      expect(game.playing?).to be false
+
+      turn = create(:turn, game: game, user: user_2)
+      expect(game.playing?).to be true
+    end
+  end
 end
