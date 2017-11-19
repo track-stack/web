4c0ba13c5a8d8f3b99373d10ab69a52ccc1c61d8
Mike Kavouras <kavourasm@gmail.com>
2017-11-18 23:56:54 -0600

    show score

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -6,11 +6,21 @@ class GameSerializer < ActiveModel::Serializer
   def players
     user_games = UserGame.includes(:user).where(game_id: object.id)
     viewer_user_game = user_games.detect { |user_game| user_game.user_id == viewer_id.to_i }
-    opponent_user_game = (user_games. - [viewer_user_game]).first
+    opponent_user_game = (user_games - [viewer_user_game]).first
+
+    # TODO: Where to put this
+    winners = object.stacks.map(&:winner).compact
+    grouped = winners.group_by(&:user_id)
+    keys = grouped.keys
+    keys.each do |key|
+      value = grouped[key]
+      puts value
+      grouped[key] = value.reduce(0) { |sum, val| val.score + sum }
+    end
 
     {
-      viewer: UserSerializer.new(viewer_user_game.user),
-      opponent: UserSerializer.new(opponent_user_game.user)
+      viewer: UserSerializer.new(viewer_user_game.user, score: grouped[viewer_id]),
+      opponent: UserSerializer.new(opponent_user_game.user, score: grouped[opponent_user_game.user_id])
     }
   end
 
