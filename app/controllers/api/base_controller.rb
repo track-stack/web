19f373c5822f338baa0cd1085798ca523c65e39d
Mike Kavouras <kavourasm@gmail.com>
2018-03-18 11:45:15 -0500

    api controller current user

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -11,5 +11,10 @@ module Api
     def current_application
       @current_application ||= Doorkeeper::Application.find_by(uid: params[:app_id])
     end
+
+    def current_user
+      return unless doorkeeper_token
+      @current_user ||= User.find(doorkeeper_token.resource_owner_id)
+    end
   end
 end
