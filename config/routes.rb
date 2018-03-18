3730dce26ce02c26c311dc6ca228a05f069a675c
Mike Kavouras <kavourasm@gmail.com>
2018-03-18 11:46:29 -0500

    Add route for dashboard/index

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "/auth/create", to: "auth#create"
+      get "/dashboard", to: "dashboard#index"
     end
   end
 end
