c258b2bc9e9d2399b9ec925ace3c2cf8ad0d0201
Mike Kavouras <kavourasm@gmail.com>
2018-03-18 16:55:19 -0500

    remove unnecessary user_game code

--- app/models/serializable/game.rb
+++ app/models/serializable/game.rb
@@ -8,27 +8,17 @@ module Serializable
     end
 
     def players
-      user_games = UserGame.includes(:user).where(game_id: game.id)
-      viewer_user_game = user_games.detect { |user_game| user_game.user_id == viewer.id.to_i }
-      opponent_user_game = (user_games - [viewer_user_game]).first
+      scores = scores_by_user_id
+      opponent = (game.players - [viewer]).first
 
-      # TODO: Where to put this
-      winners = game.stacks.map(&:winner).compact
-      grouped = winners.group_by(&:user_id)
-      user_ids = grouped.keys
-      user_ids.each do |user_id|
-        stacks = grouped[user_id]
-        grouped[user_id] = stacks.reduce(0) { |sum, stack| stack.score + sum }
-      end
-
-      viewer_hash = UserSerializer.new(viewer_user_game.user).to_hash
+      viewer_hash = UserSerializer.new(viewer).to_hash
       viewer_attrs = viewer_hash[:data][:attributes]
-      opponent_hash = UserSerializer.new(opponent_user_game.user).to_hash
+      opponent_hash = UserSerializer.new(opponent).to_hash
       opponent_attrs = opponent_hash[:data][:attributes]
 
       {
-        viewer: viewer_attrs.merge(score: grouped[viewer.id]),
-        opponent: opponent_attrs.merge(score: grouped[opponent_user_game.user_id])
+        viewer: viewer_attrs.merge(score: scores[viewer.id]),
+        opponent: opponent_attrs.merge(score: scores[opponent.id])
       }
     end
 
