c1254d555c1057fdf49670c7e73121c9dd518c3d
Mike Kavouras <kavourasm@gmail.com>
2018-04-08 13:57:33 -0500

    GamesController#turn

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,14 +5,50 @@ class Api::V1::GamesController < ::Api::BaseController
   before_action :require_player_in_game!, only: [:show, :turn]
 
   def show
-    serializable = Serializable::Game.new(game: game, viewer: current_user)
+    serializable = Serializable::Game.new(game: @game, viewer: current_user)
     serialized = GameSerializer.new(serializable).to_hash[:data][:attributes]
     render json: { game: serialized }
+  end
+
+  def turn
+    turn = Turn.create(
+      user: current_user,
+      game: @game,
+      stack: stack,
+      answer: params[:answer],
+      match: sanitize_match(params[:match])
+    )
+
+    if params[:game_over] && stack.can_end?
+      stack.mark_winner!(current_user)
+    end
+
+    if turn.valid?
+      serializable = Serializable::Game.new(game: @game.reload, viewer: current_user)
+      serialized = GameSerializer.new(serializable).to_hash[:data][:attributes]
+      render json: { game: serialized }
+    else
+      render json: { errors: turn.errors }, status: :bad_request
+    end
   end
 
   private
 
   def game
+  def sanitize_match(match)
+    name = match["name"]
+    artist = match["artist"]
+    image = match["image"].last["#text"]
+
+    { name: name, artist: artist, image: image }
+  end
+
+  def stack
+    return @stack if defined?(@stack)
+    @stack = Stack.find_by(id: params[:stack_id]) if params[:stack_id]
+    @stack ||= @game.stacks.last
+  end
+
   def require_game!
     includes = {stacks: [{turns: [:user]}]}
     unless @game = Game.includes(includes).find_by(id: params[:id])
