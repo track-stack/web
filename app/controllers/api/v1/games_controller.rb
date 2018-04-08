d737e4e5b2fe6a1a9813df6ab7a767856b1e8c17
Mike Kavouras <kavourasm@gmail.com>
2018-04-08 13:57:12 -0500

    GamesController validation methods

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::GamesController < ::Api::BaseController
   before_action :doorkeeper_authorize!
   before_action :require_application!
+  before_action :require_game!, only: [:show, :turn]
+  before_action :require_player_in_game!, only: [:show, :turn]
 
   def show
     serializable = Serializable::Game.new(game: game, viewer: current_user)
@@ -11,7 +13,16 @@ class Api::V1::GamesController < ::Api::BaseController
   private
 
   def game
+  def require_game!
     includes = {stacks: [{turns: [:user]}]}
-    @game ||= Game.includes(includes).find_by(id: params[:id])
+    unless @game = Game.includes(includes).find_by(id: params[:id])
+      render_404
+    end
+  end
+
+  def require_player_in_game!
+    unless @game.user_games.map(&:user_id).include? current_user.id
+      return render_404
+    end
   end
 end
