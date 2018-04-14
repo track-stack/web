1cb805f4bc47007da2b42a7c3bc56061ce626df5
Mike Kavouras <kavourasm@gmail.com>
2018-04-13 20:39:33 -0500

    create table devices

---
+++ db/migrate/20180414013421_create_devices.rb
@@ -0,0 +1,15 @@
+class CreateDevices < ActiveRecord::Migration[5.1]
+  def change
+    create_table :devices do |t|
+      t.string :device_id
+      t.string :apns_token
+      t.integer :user_id
+
+      t.timestamps
+    end
+
+    add_index :devices, :device_id
+    add_index :devices, :apns_token
+    add_index :devices, :user_id
+  end
+end
